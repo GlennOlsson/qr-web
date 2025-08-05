6aa259b5cc50c6b6b6e87d0c05a2108b0b3fb61d
Glenn Olsson <glennholsson@gmail.com>
2025-08-04 22:20:36 -0700

    fix styling

--- site/qr-site/src/App.jsx
+++ site/qr-site/src/App.jsx
@@ -1,23 +1,55 @@
 import "./App.css"
-import { useLocation } from "react-router"
+// import { useLocation } from "react-router"
 
 function App() {
-  let location = useLocation();
-  let state = location.state;
+  // let location = useLocation();
+  // let state = location.state;
 
-  console.log("state: ", state);
+  // console.log("state: ", state);
 
-  let item;
-  if (state && state.item)
-    item = state.item;
-  else
-    item = "something...";
+  // let item;
+  // if (state && state.item)
+  //   item = state.item;
+  // else
+  //   item = "something...";
+  let item = "backpack";
 
   return (
-    <>
-      You found my {item}
-      Glenn; Traveler, weirdo, self-proclaimed QR code enthusiast
-    </>
+    <div className="main-frame">
+      <p className="title">
+        You found my {item}!
+      </p>
+      <div className="body-container">
+        <div>
+          <p className="body-text">
+            Glenn;
+          </p>
+          <div className="body-text">
+            <p>
+              {"→ "}Traveler,
+            </p>
+            <p>
+              {"→ "}Weirdo,
+            </p>
+            <p>
+              {"→ "}QR code enthusiast  
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="icon-container">
+        <a href="https://instagr.am/glennminusgbg">
+          <div className="icon">
+              <img src="/instagram.png" />
+          </div>
+        </a>
+        <a href="https://linkedin.com/in/glenn-olsson/">
+          <div className="icon">
+              <img src="/linkedin.png" />
+          </div>
+        </a>
+      </div>
+    </div>
   )
 }
 
