11cd9962bbfd6b916d93056003aef089653b6a03
Glenn Olsson <glennholsson@gmail.com>
2025-08-04 20:19:55 -0700

    add react router dom

--- site/qr-site/src/main.jsx
+++ site/qr-site/src/main.jsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const NoPath = () => {
+  return (
+    <div>
+      There's no QR code like that!
+    </div>
+  )
+}
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/backp0" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NoPath />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
